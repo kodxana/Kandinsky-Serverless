a63ca04759056fb16536f2115b31a80025126f86
kodxana <romanrex19@int.pl>
2023-04-24 10:14:20 +0200

    Add support for negative prompts

--- rp_handler.py
+++ rp_handler.py
@@ -3,17 +3,43 @@
 import base64
 import io
 import os
-from kandinsky2 import get_kandinsky2
+from kandinsky2 import CONFIG_2_1, Kandinsky2_1 
+from omegaconf.dictconfig import DictConfig
 import runpod
 from runpod.serverless.utils import rp_download, rp_upload, rp_cleanup
 from runpod.serverless.utils.rp_validator import validate
+from copy import deepcopy
 
-model = get_kandinsky2(
-    'cuda',
+# Add the create_model function to your code
+def create_model(unet_path, cache_root, task_type, device, use_fp16=True):
+    config = DictConfig(deepcopy(CONFIG_2_1))
+    cache_dir = os.path.join(cache_root, "2_1")
+
+    config["model_config"]["up"] = False
+    config["model_config"]["use_fp16"] = use_fp16
+    config["model_config"]["inpainting"] = False
+    config["model_config"]["cache_text_emb"] = False
+    config["model_config"]["use_flash_attention"] = False
+
+    config["tokenizer_name"] = os.path.join(cache_dir, "text_encoder")
+    config["text_enc_params"]["model_path"] = os.path.join(cache_dir, "text_encoder")
+    config["prior"]["clip_mean_std_path"] = os.path.join(cache_dir, "ViT-L-14_stats.th")
+    config["image_enc_params"]["ckpt_path"] = os.path.join(cache_dir, "movq_final.ckpt")
+
+    model_path = os.path.join(cache_dir, "decoder_fp16.ckpt") if unet_path is None else unet_path
+    prior_path = os.path.join(cache_dir, "prior_fp16.ckpt")
+
+    return Kandinsky2_1(config, model_path, prior_path, device, task_type=task_type)
+
+# Set the path to your local UNet model
+unet_path = "/app/kandinsky2/2_1/decoder_fp16.ckpt"
+
+model = create_model(
+    unet_path=unet_path,
+    cache_root='/app/kandinsky2',
     task_type='text2img',
-    cache_dir='/app/kandinsky2',
-    model_version='2.1',
-    use_flash_attention=False
+    device='cuda',
+    use_fp16=True
 )
 
 INPUT_SCHEMA = {
@@ -60,6 +86,16 @@ INPUT_SCHEMA = {
         'type': str,
         'required': False,
         'default': "5"
+    },
+    'negative_prior_prompt': {
+        'type': str,
+        'required': False,
+        'default': ""
+    },
+    'negative_decoder_prompt': {
+        'type': str,
+        'required': False,
+        'default': ""
     }
 }
 
@@ -82,7 +118,9 @@ def generate_image(job):
         w=validated_input['w'],
         sampler=validated_input['sampler'],
         prior_cf_scale=validated_input['prior_cf_scale'],
-        prior_steps=validated_input['prior_steps']
+        prior_steps=validated_input['prior_steps'],
+        negative_prior_prompt=validated_input['negative_prior_prompt'],
+        negative_decoder_prompt=validated_input['negative_decoder_prompt']
     )
 
     # Save the generated image to a file
